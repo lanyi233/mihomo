e34f8c1a82cca0223176ff45e3693faf9d1d76d2
lanyi233 <lanyi233@qq.com>
2026-09-03 07:11:50 +0800

    edit(update.core): edit update api endpoint

--- component/updater/update_core.go
+++ component/updater/update_core.go
@@ -24,11 +24,11 @@ import (
 )
 
 const (
-	baseReleaseURL    = "https://github.com/MetaCubeX/mihomo/releases/latest/download/"
-	versionReleaseURL = "https://github.com/MetaCubeX/mihomo/releases/latest/download/version.txt"
+	baseReleaseURL    = "https://github.com/lanyi233/mihomo/releases/latest/download/"
+	versionReleaseURL = "https://github.com/lanyi233/mihomo/releases/latest/download/version.txt"
 
-	baseAlphaURL    = "https://github.com/liuran001/mihomo/releases/download/Prerelease-Alpha/"
-	versionAlphaURL = "https://github.com/liuran001/mihomo/releases/download/Prerelease-Alpha/version.txt"
+	baseAlphaURL    = "https://github.com/lanyi233/mihomo/releases/download/Prerelease-Alpha/"
+	versionAlphaURL = "https://github.com/lanyi233/mihomo/releases/download/Prerelease-Alpha/version.txt"
 
 	// MaxPackageFileSize is a maximum package file length in bytes. The largest
 	// package whose size is limited by this constant currently has the size of
